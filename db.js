de27a653b9eecf495305da466cb2853cad4b57f3
kumar <santanupaikaray23@gmail.com>
2021-09-27 12:20:14 +0530

    add all files

---
+++ db.js
@@ -0,0 +1,4 @@
+// var mongoose = require('mongoose');
+// mongoose.connect(`mongodb+srv://dev:mongo123@cluster0.f8vmc.mongodb.net/nareshit?retryWrites=true&w=majority`)
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://naturewithcode:nature123@cluster0.yf62c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
